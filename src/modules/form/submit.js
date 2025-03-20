ab552c8141b5091f853f537b25ffdad0bc701cc4
Danielldiniz1 <fortedinizdaniel@gmail.com>
2025-03-20 20:29:17 -0300

    criando interçoes com as datas

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -2,8 +2,13 @@ import dayjs from 'dayjs';
 const form = document.querySelector('form');
 const selectedDate = document.getElementById('date');
 
-//Carrega a data atual.
-selectedDate.value = dayjs(new Date()).format('YYYY-MM-DD');
+//Data atual para input
+const inputToday = dayjs(new Date()).format('YYYY-MM-DD');
+
+
+//Carrega a data atual,Define a data minima para agendamento.
+selectedDate.value = inputToday;
+selectedDate.min = inputToday;
 
 form.onsubmit = (event) => {
     event.preventDefault();
