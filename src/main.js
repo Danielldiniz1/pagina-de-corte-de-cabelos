18c2631e26148cf289649eee733f800cfa8e51ed
Danielldiniz1 <fortedinizdaniel@gmail.com>
2025-03-20 20:22:02 -0300

    iniciando

---
+++ src/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//Configuração do Dayjs
+import './libs/dayjs';
+
+
+//CSS
+import './styles/global.css';
+import './styles/form.css';
+import './styles/schedule.css';
+
+//JS
+import './modules/form/submit';
---
+++ src/modules/form/submit.js
@@ -0,0 +1,13 @@
+import dayjs from 'dayjs';
+const form = document.querySelector('form');
+const selectedDate = document.getElementById('date');
+
+//Carrega a data atual.
+selectedDate.value = dayjs(new Date()).format('YYYY-MM-DD');
+
+form.onsubmit = (event) => {
+    event.preventDefault();
+
+    
+   
+}
