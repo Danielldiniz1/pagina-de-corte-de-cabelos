a77d59afe22ae8afc5a18a02a89f3762758e3119
Danielldiniz1 <fortedinizdaniel@gmail.com>
2025-03-20 22:42:56 -0300

    Renderizando os agendamentos do dia

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,9 +1,20 @@
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
 import { hoursLoad } from "../form/hours-load.js";
+import { schedulesShow } from "./show.js";
 //Seleciona o input de data
 const selectedDate = document.querySelector('#date');
-export function schedulesDay(){
+export async function schedulesDay(){
     //Obtem a data do input
     const date = selectedDate.value;
+
+    //Busca na API os agendamentos da data selecionada
+    const dailySchedules = await scheduleFetchByDay({date});
+
+    //Exibe os agendamentos na tela
+    schedulesShow({ dailySchedules });
+    
+
+
     //Carrega os horários disponíveis
     hoursLoad({date});
 }
