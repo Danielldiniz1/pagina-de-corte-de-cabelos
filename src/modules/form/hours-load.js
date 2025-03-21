27916d71f2ff99d01a660dce243514c99c455f26
Danielldiniz1 <fortedinizdaniel@gmail.com>
2025-03-20 21:03:35 -0300

    Renderizando os horários

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,13 +1,16 @@
 import dayjs from 'dayjs';
 import {openingHours} from '../../utils/opening-hours.js';
 
+
+const hours = document.getElementById('hours');
+
 export function hoursLoad({ date }){
    const opening = openingHours.map((hour) =>{
     //Reucupera somente a hora
     const [scheduleHour] = hour.split(":")
 
     //Adiciona a hora na date e verifica se está no passado
-    const isHourPast = dayjs(date).add(scheduleHour, 'hours').isAfter(dayjs())
+    const isHourPast = dayjs(date).add(scheduleHour, 'hours').isBefore(dayjs())
   
 
     //Define se o horario esta disponivel
@@ -16,5 +19,15 @@ export function hoursLoad({ date }){
         hour,
         available: !isHourPast,
     }
+ })
+
+ //Renderiza os horarios na tela
+ opening.forEach(({hour, available}) =>{
+    const li = document.createElement('li');
+
+    li.classList.add("hour");
+    li.classList.add(available ? "hour-available" : "hour-unavailable");
+    li.textContent = hour;
+    hours.append(li);
  })
 }
