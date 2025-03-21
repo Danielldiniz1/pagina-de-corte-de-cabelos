b438c5cf790320068f8eb29c12318f9abf7d5cc0
Danielldiniz1 <fortedinizdaniel@gmail.com>
2025-03-20 21:25:04 -0300

    Enviando horarios para a API

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs';
 const form = document.querySelector('form');
+const clientName = document.getElementById('client');
 const selectedDate = document.getElementById('date');
 
 //Data atual para input
@@ -13,6 +14,45 @@ selectedDate.min = inputToday;
 form.onsubmit = (event) => {
     event.preventDefault();
 
-    
+    try {
+        const name = clientName.value.trim();
+
+
+        if(!name){
+            return alert("Informe o nome do cliente");
+        }
+
+        //Recupera o horario selecionado
+        const hourSelected = document.querySelector('.hour-selected');
+       
+        //Recupera o horário selecionado
+        if(!hourSelected){
+            return alert("Selecione um horário");
+        }
+
+        
+  //Recupera somente o horario
+  const [hour] = hourSelected.innerHTML.split(":");
+
+
+  //Insere a hora e data
+  const when = dayjs(selectedDate.value).add(hour, 'hours');
+  console.log(when);
+
+  //Dera um ID
+  const id = new Date().getTime();
+
+  console.log({
+    id,
+    name,
+    when,
+  });
+  
+  
+    } catch (error) {
+        alert("Não foi possível realizar o agendamento");
+        console.log(error);
+        
+    }
    
 }
